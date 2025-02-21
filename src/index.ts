14f97c7190992372eb15dad74a37ab9d8a2d1694
Adriano Miranda <adrjmiranda@gmail.com>
2025-02-21 00:08:15 -0300

    style: added animation with scrollreveal

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,30 @@
 /**
  * Scroll Reveal Animation
  */
+import ScrollReveal from 'scrollreveal';
+
+ScrollReveal().reveal('.title', {
+	opacity: 0,
+	reset: false,
+	duration: 1200,
+	delay: 100,
+	easing: 'ease-in-out',
+});
+
+ScrollReveal().reveal('.image-content', {
+	opacity: 0,
+	reset: true,
+	duration: 1750,
+	delay: 250,
+	easing: 'ease-in-out',
+});
+
+ScrollReveal().reveal('.form-content', {
+	opacity: 0,
+	reset: true,
+	duration: 1550,
+	delay: 200,
+	origin: 'bottom',
+	distance: '-20%',
+	easing: 'ease-in-out',
+});
